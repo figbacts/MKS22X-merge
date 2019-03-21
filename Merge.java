a06baa3c4bdc97cae2f80d3b41a55a12a6184870
Jack Lam <jlam00@stuy.edu>
2019-03-21 01:12:06 -0400

    added inserttion sort

--- Merge.java
+++ Merge.java
@@ -1,30 +1,21 @@
 import java.util.*;
 public class Merge{
   public static void mergesort(int[]data){
-
     mergesortH(data,0,data.length-1);
   }
   public static void mergesortH(int[]data, int lo, int hi){
-    if(lo >= hi){
+    int middle = ((lo + hi) /2);
+    if (hi - lo < 180){
+      insertionsort(data,lo,hi);
       return;
     }
-    int middle = ((lo + hi) /2);
-    /*
-    System.out.println(middle);
-
-    int [] left = new int[middle+1];
-    for(int i = 0; i < middle +1; i ++){
-      left[i] = data[i];
-    }
-    int [] right = new int[data.length - middle -1];
-    for(int i = middle+1; i < data.length ; i ++){
-      right[i-middle -1] = data[i];
-    }
-    System.out.println(Arrays.toString(left));
-    System.out.println(Arrays.toString(right));
-    */
+    //if(hi-lo < 50){
+    //   insertionsort(data,lo,hi);
+    //}
+    //else{
     mergesortH(data,lo,middle);
     mergesortH(data,middle +1, hi);
+  //}
     merge(data,lo,middle,hi);
   }
   public static void merge(int[] data, int lo, int middle, int hi){
@@ -92,12 +83,28 @@ public class Merge{
     }
 */
   }
+  public static void insertionsort(int[] data, int lo,int hi){
+    int len = hi - lo + 1;
+  for (int i = 1; i<len; i++){
+    int hold = data[lo + i];
+    int index = i;
+  while (index > 0 &&  hold <data[lo + index -1]){
+    data[lo + index] = data[lo + index -1];
+    index --;
+  }
+  data[lo + index] = hold;
+  }
+}
+
   public static void main(String[] args) {
     //int[] orginal = new int [10];
-    int[] data1 = {2,5,7,3,4,1,0,9,8};
+    int[] data1 = {99,88,77,44,55,44,22,33,23123,4125347,65865345};
     //int[] data2 = {1,3,5,7};
     //merge(orginal, data1,data2);
-    mergesort(data1);
+    //mergesort(data1);
+    //System.out.println(Arrays.toString(data1));
+    insertionsort(data1,1,11);
     System.out.println(Arrays.toString(data1));
   }
+
 }
